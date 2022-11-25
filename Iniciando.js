4eecf5ac7c729172dc9b7b22d91f49f974b0ad63
Maciel Ribeiro de Oliveira <38005099+Maxnegrao33@users.noreply.github.com>
2022-11-25 15:32:12 -0300

    Add files via upload

---
+++ Iniciando.js
@@ -0,0 +1,40 @@
+//Cria uma pequena caixa com um texto e um botão sobre a tela
+    alert("Iniciando, Estou funcionando")
+
+//Interessante enquanto trabalha como JavaScript, deixar o Console aberto
+    console.log("Olá Mundo!")
+
+//Criando as Variáveis
+    let nome = "Wilson" //variável em string (caracteres)
+    let idade = 17  //variável em number (número)
+    let logado = true
+    let lista = ["farinha, ovo, leite, fermento, óleo, açucar"] //array tipo lista
+    let nomeCompleto = { nome: "Wilson", idade: 17} //objeto
+    //A definição de variável no JavaScript usa a notação camelCase
+    //Na prática, tanto objeto como array são considerados como objetos
+
+ //Chamando as variáveis  
+    console.log(typeof nome) 
+    console.log(typeof idade)
+    console.log(typeof logado)
+    console.log(typeof lista)
+    console.log(typeof nomeCompleto)
+
+
+
+//Tipos de Dados no JavaScript
+/* JavaScript usa padrão internacional, que são números com pontos
+Dados em string (Caracteres)
+Dados em number (Números)
+Dados em boolean (True or false)
+Dados em Array []
+Dados em Objeto {}
+*/
+
+
+
+
+
+
+
+
